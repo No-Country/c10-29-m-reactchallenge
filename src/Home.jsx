c8a8edc3f421eec496ee1398cc4bef2b3132c210
loli178aliberti_lorena@yahoo.com.ar <you@example.com>
2023-03-25 17:42:10 -0300

    first login code so Miguel can connect firebasn

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 
 const Home = () => {
-<<<<<<< HEAD
-  return <div>chau</div>;
-=======
   return (
-    <div >
+    <div>
       No se olviden ordenar todo en carpetas. Ya les deje una foto de guia!
     </div>
   );
->>>>>>> de2e237b8c996b7cc1410a1372f045be46b7c303
 };
 
 export default Home;
