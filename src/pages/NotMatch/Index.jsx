3d337e1f05392d2cba65d04d2eca5f4440b9727f
MatiasMass <matiasmass14@gmail.com>
2023-03-25 23:36:44 -0300

    fixing not found page

--- src/pages/NotMatch/Index.jsx
+++ src/pages/NotMatch/Index.jsx
@@ -2,14 +2,16 @@ import { Link } from 'react-router-dom'
 import './Index.css'
 const NotMatch = () => {
   return (
-    <div class="mainbox">
-      <div class="err">404</div>
-      <div class="msg">
-        Maybe this page moved? Got deleted? Is hiding out in quarantine? Never
-        existed in the first place?
-        <p>
-          Let's go <Link to="/">Home</Link> and try from there.
-        </p>
+    <div className="container-not-found">
+      <div class="mainbox">
+        <div class="err">404</div>
+        <div class="msg">
+          Maybe this page moved? Got deleted? Is hiding out in quarantine? Never
+          existed in the first place?
+          <p>
+            Let's go <Link to="/">Home</Link> and try from there.
+          </p>
+        </div>
       </div>
     </div>
   );
