2805cdfaa664ecea31e20f0e76e2bfb23d461914
MatiasMass <matiasmass14@gmail.com>
2023-03-26 16:18:49 -0300

    adding formik component and styles in css file

--- src/pages/ContactUs/Index.jsx
+++ src/pages/ContactUs/Index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from "../../layouts/Navbar/Index";
 import Footer from "../../layouts/Footer/Index";
+import Form from "./components/Form";
 import "./Index.css";
 
 function Index() {
   return (
     <div>
       <Navbar />
-
+      <Form />
       <Footer />
     </div>
   )
