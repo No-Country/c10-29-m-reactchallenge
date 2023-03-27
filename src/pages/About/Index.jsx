18fe31be2a8f8362f47e8e8ef4b1ca816d2f192f
Nancy Clavijo Varela <clavijovarela@gmail.com>
2023-03-27 12:05:30 -0500

    formEventFormik

--- src/pages/About/Index.jsx
+++ src/pages/About/Index.jsx
@@ -1,12 +1,13 @@
 import Navbar from "../../layouts/Navbar/Index";
 import Footer from "../../layouts/Footer/Index";
+import CreateEventForm from "../../components/CreateEventForm";
 import "./Index.css";
 
 function Index() {
   return (
     <div>
       <Navbar />
-
+     
       <Footer />
     </div>
   )
