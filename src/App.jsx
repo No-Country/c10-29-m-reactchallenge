d00fb7a8044925b2b778145ce0b672166d77cf36
Miguel1488 <118023734+Miguel1488@users.noreply.github.com>
2023-04-03 12:16:34 -0500

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 import DefaultLayout from './layouts/DefaultLayout';
-// import Upload from './pages/Seller/Sell/Upload';
+
 
 
 const App = () => {
   return (
-    // <Upload/>
+    
     <DefaultLayout />
     
   )
