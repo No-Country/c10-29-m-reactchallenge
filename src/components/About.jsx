d91caa8ccfa1728130a68fe842a77743d7efad36
Kova <ferkovalink@gmail.com>
2023-04-04 02:43:19 -0300

    css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,11 @@ const AboutUs = () => {
   return (
     <div class="about-us">
   <h2>Sobre Nosotros</h2>
-  <p>Somos una empresa dedicada a brindar la mejor experiencia en la venta de tickets de eventos. Nos esforzamos por ofrecer un servicio rápido, eficiente y seguro para todos nuestros clientes. Contamos con un equipo de profesionales altamente capacitados que están dispuestos a ayudarte en todo lo que necesites.</p>
-  <p>En nuestro equipo, contamos con expertos en tecnología, marketing y atención al cliente trabajando juntos para brindar la mejor experiencia posible a nuestros clientes..</p>
+  <p>En Bypass, nuestro objetivo es hacer que la compra y venta de entradas digitales para eventos sea más fácil y accesible para todos. Ya sea que estés buscando entradas para un concierto, un partido de deportes, un festival de música o cualquier otro evento, estamos aquí para ayudarte a conectarte con vendedores confiables y hacer que la experiencia de comprar y vender entradas sea lo más simple posible.</p>
+
+  <p>Nuestro equipo está compuesto por profesionales altamente capacitados y apasionados por los eventos en vivo y la tecnología, lo que nos permite ofrecer una plataforma fácil de usar, segura y confiable para comprar y vender entradas digitales.</p>
+
+  <p>Con Bypass, puedes estar seguro de que estás obteniendo entradas auténticas y legítimas para el evento que deseas asistir. Además, nuestro sistema de pagos seguro y encriptado te brinda tranquilidad al momento de realizar transacciones en líne</p>
   
   <div>
         <Link to={'/'}>
