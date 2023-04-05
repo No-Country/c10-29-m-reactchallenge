5a2e44b10c2a1a1025b520025636573a31b48216
Miguel1488 <118023734+Miguel1488@users.noreply.github.com>
2023-04-05 09:27:15 -0500

    pay

--- src/pages/Cart/Index.jsx
+++ src/pages/Cart/Index.jsx
@@ -1,11 +1,12 @@
 import Template from '../../layouts/Template/Index'
 import Cart from '../../components/Cart'
-
+import Pay from "../../pays/Pay"
 function Index() { 
   
   return (
     <Template>
         <Cart />
+        <Pay />
     </Template>
   )
 }
