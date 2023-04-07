eca30d3b7cddd544af6fd3d61e35b4752b5f7301
MatiasMass <matiasmass14@gmail.com>
2023-04-07 18:24:15 -0300

    styling navbar and buyer pages

--- src/pages/Buyer/Purchases/Index.jsx
+++ src/pages/Buyer/Purchases/Index.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchGetAllPurchasesByUserId } from "../../../redux/features/purchases/purchasesSlice";
 import Template from "../../../layouts/Template/Index";
@@ -47,7 +48,11 @@ function Index() {
                       <td>{purchase.price}</td>
                       <td>
                         <button className="btn btn-primary">Ver</button>
+                        <Link to={`/purchases/${purchase.uid}`}>
+                          <button className="btn btn-primary">Descargar</button>
+                        </Link>
                       </td>
+                      
                     </tr>
                   );
                 })}
